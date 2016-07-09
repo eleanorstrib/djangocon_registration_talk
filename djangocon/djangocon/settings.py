0ef7845b3a0067a27de8a75771d8a3f083345809
eleanorstrib <eleanor@sloan.mit.edu>
2016-07-08 21:15:17 -0700

    installed django-registration-redux, updated registration.txt, added required installed_apps to settings.py

--- djangocon/djangocon/settings.py
+++ djangocon/djangocon/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.sites',
+    'registration',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
